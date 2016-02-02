29ba573199d0bfc58833a3196dd505806654462b
ultimawar <y.inuyasha@gmail.com>
2016-02-02 13:08:25 -0500

    I added a comment with my name as the author

--- src/SequenceLoader.java
+++ src/SequenceLoader.java
@@ -3,7 +3,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import javax.swing.JFileChooser;
-
+/**
+ * 
+ * @author keshavdial
+ *
+ */
 public abstract class SequenceLoader {
 
 	protected String sequence;
